c9723c1b3bab6046b9782be789b322650301007c
ahmadhudhayfa <ahmad.hudhayfa@gmail.com>
2024-07-06 23:35:36 +0800

    add css and js

---
+++ script.js
@@ -0,0 +1,25 @@
+const students = [];
+
+function addStudent() {
+  const name = prompt("Enter the student's name:");
+  const grade = parseInt(prompt("Enter the student's grade:"));
+  const className = prompt("Enter the student's class:");
+  const student = {
+    name: name,
+    grade: grade,
+    className: className,
+  };
+
+  students.push(student);
+  alert('Student added!')
+}
+
+function displayStudents() {
+  let studentList = "List of students:\n";
+  for (const student of students) {
+    studentList += `${student.name} from class ${student.className} : ${student.grade}\n`;
+  }
+  alert(studentList);
+}
+
+
